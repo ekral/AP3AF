302401781ce96076df36ed0f95e5a31088839435
Erik Král <ekral@utb.cz>
2020-09-23 10:27:15 +0200

    Add project files.

---
+++ MobilniAplikace2/MobilniAplikace2/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MobilniAplikace2
+{
+    public partial class MainPage : ContentPage
+    {
+        private int counter = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            ++counter;
+            mujLabel.Text = counter.ToString();
+        }
+    }
+}
